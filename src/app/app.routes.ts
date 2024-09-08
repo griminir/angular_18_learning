1a1ec23842a897e5bd4c1a6704e45e57f0784a84
griminir <viktor.elliot.king@gmail.com>
2024-09-08 18:19:24 +0200

    added routing for client project

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { MasterComponent } from './components/master/master.component';
 import { EmployeeComponent } from './components/employee/employee.component';
 import { ClientComponent } from './components/client/client.component';
+import { ClientProjectComponent } from './components/client-project/client-project.component';
 
 export const routes: Routes = [
   {
@@ -21,4 +22,8 @@ export const routes: Routes = [
     path: 'client',
     component: ClientComponent,
   },
+  {
+    path: 'client-project',
+    component: ClientProjectComponent
+  }
 ];
